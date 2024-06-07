208248e01dfbfdb2634ecb8601a66ffdda750350
PedroSantana <phsantana99@gmail.com>
2024-06-07 02:56:28 -0300

    fix: fixes for next auth implementation

--- src/api/services/Login.service.ts
+++ src/api/services/Login.service.ts
@@ -2,7 +2,7 @@ import { destroyCookie } from 'nookies'
 import { prismaClient } from '../../database/prismaClient'
 import { IUser } from '../../interfaces'
 import { comparePasswords } from '../../utils/bcrypt'
-import { generateToken } from '../../utils/jwt'
+// import { generateToken } from '../../utils/jwt'
 import { UserService } from './User.service'
 import { Response } from 'express'
 
@@ -10,26 +10,38 @@ export class LoginService {
   private userService = new UserService()
 
   public async signIn({ email, password }: IUser) {
-    const { status, message, data } = await this.userService.readByEmail(email)
-    if (!data) return { status, message }
+    const { status, data } = await this.userService.readByEmail(email)
+    if (!data) return { status, message: 'As credenciais estão incorretas' }
 
     const user = await this.userService.readPassword(email)
     if (!user || !user.data?.password)
-      return { status: 404, message: 'Usuário não encontrado' }
+      return { status: 400, message: 'As credenciais estão incorretas' }
 
     const passwordsCheck = await comparePasswords(password, user.data?.password)
     if (!passwordsCheck)
-      return { status: 401, message: 'A senha informada está incorreta' }
+      return { status: 400, message: 'As credenciais estão incorretas' }
 
-    const token = await generateToken({
-      id: data.id,
-      email: data.email,
-      name: data.name,
-      phone: data.phone,
-    })
+    if (passwordsCheck) {
+      // const token = await generateToken({
+      //   id: data.id,
+      //   email: data.email,
+      //   name: data.name,
+      //   phone: data.phone,
+      // })
 
-    await prismaClient.$disconnect()
-    return { status: 200, message: 'Login efetuado com sucesso', token }
+      await prismaClient.$disconnect()
+      return {
+        status: 200,
+        message: 'Login efetuado com sucesso',
+        userData: {
+          id: data.id,
+          email: data.email,
+          name: data.name,
+        },
+      }
+    }
+
+    return { status: 500, message: 'Ocorreu um erro inesperado' }
   }
 
   // ///////////////////////////////////////////////////////////////
